3ac37ef3ff947721baaf473c777c08a432a7ee0f
fokrule <fokrule.hasan@gmail.com>
2021-04-03 23:08:09 +0600

    main body upddate

--- components/Mainbody.js
+++ components/Mainbody.js
@@ -2,31 +2,31 @@ const Mainbody = () => {
 	return (
 		<div>
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://github.com/fokrule" className="card">
+            <h3>GitHub &rarr;</h3>
+            <p>Have a look into my gitHub projects.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://www.linkedin.com/in/a-t-m-fokrule-hasan-a052a696/" className="card">
+            <h3>linkedin &rarr;</h3>
+            <p>Vist my Linkedin profile and make a connection with me.</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://medium.com/@forhad095"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Medium &rarr;</h3>
+            <p>Discover my Medium profile.</p>
+          </a>
+
+          <a
+            href="https://stackoverflow.com/users/6731111/fokrule"
+            className="card"
+          >
+            <h3>Stackoverflow &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Do a tour into my stackoverflow world.
             </p>
           </a>
         </div>
